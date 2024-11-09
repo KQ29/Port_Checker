c3cc4e9b687c0f797f4f81c34a005dc95f58066a
KQ29 <ibragimovkamronbek2005@gmail.com>
2024-11-09 18:26:25 +0000

    common_ports creation

---
+++ common_ports.py
@@ -0,0 +1,21 @@
+# Dictionary of common ports and services
+common_ports = {
+    20: 'FTP',
+    21: 'FTP',
+    22: 'SSH',
+    23: 'Telnet',
+    25: 'SMTP',
+    53: 'DNS',
+    80: 'HTTP',
+    110: 'POP3',
+    143: 'IMAP',
+    443: 'HTTPS',
+    465: 'SMTPS',
+    993: 'IMAPS',
+    995: 'POP3S',
+    3306: 'MySQL',
+    3389: 'RDP',
+    5432: 'PostgreSQL',
+    5900: 'VNC',
+    8080: 'HTTP-Proxy'
+}
